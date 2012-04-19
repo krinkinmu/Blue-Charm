45747f1dbd82f4b9b0f0814a8a9589addccefa87
Кринкин Михаил <mirovingen@mirovingen-laptop.(none)>
2012-04-19 11:29:05 +0400

    Eclipse Project

---
+++ android/src/ru/spbau/bluecharm/BlueCharm.java
@@ -0,0 +1,12 @@
+package ru.spbau.bluecharm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlueCharm extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
