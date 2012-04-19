b78b1691aceaa2ec7eab956565ae4c423d20eb6b
Кринкин Михаил <mirovingen@mirovingen-laptop.(none)>
2012-04-19 12:16:24 +0400

    First View

--- android/src/ru/spbau/bluecharm/BlueCharm.java
+++ android/src/ru/spbau/bluecharm/BlueCharm.java
@@ -1,12 +1,22 @@
 package ru.spbau.bluecharm;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BlueCharm extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ArrayList<String> data = new ArrayList<String>();
+        ArrayAdapter<String> tst = new ArrayAdapter(this,
+        		android.R.layout.simple_list_item_checked, data);
+        ((ListView) findViewById(R.id.blueDevices)).setAdapter(tst);
+        tst.add("tst1");
+        tst.add("tst2");
     }
 }
