21bda89e1f0b62f308e9544af989face1de0f468
Кринкин Михаил <mirovingen@mirovingen-laptop.(none)>
2012-04-20 21:25:29 +0400

    Common BlueCharNotifier class

--- android-prerelise/BlueCharm/src/ru/spbau/bluecharm/CallsNotifier.java
+++ android-prerelise/BlueCharm/src/ru/spbau/bluecharm/CallsNotifier.java
@@ -1,48 +1,26 @@
 package ru.spbau.bluecharm;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
-public class CallsNotifier extends BroadcastReceiver {
-	public static final String TAG = "CALLS_NOTIFIER";
-	public static final String MAGIC = "BLUECHARM";
+public class CallsNotifier extends BlueCharmNotifier {
 	public static final String IN_CALL = "IN_CALL";
-	public static final char DELIMITER  = 3;
-	
+
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		Intent service = new Intent(context, BlueCharmService.class);
-		IBinder binder = peekService(context, service);
-		if (binder != null) {
-			sendMessage(intent, binder);
-		} else {
-			Log.d(TAG, "BlueCharmService isn't running");
-		}
+	protected String buildMessage(Context context, Intent intent) {
+		return intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
 	}
-	
-	private void sendMessage(Intent intent, IBinder binder) {
-		Log.d(TAG, "Intent received: " + intent.getStringExtra(TelephonyManager.EXTRA_STATE));
-		if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-			Messenger messenger = new Messenger(binder);
-			Message msg = Message.obtain(null, BlueCharmService.MSG_NOTIFY_LISTENERS, 0, 0);
-			Bundle bundle = new Bundle();
-			String data = MAGIC + DELIMITER + IN_CALL + DELIMITER
-					+ intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-			bundle.putString(null, data);
-			msg.setData(bundle);
-			try {
-				messenger.send(msg);
-			} catch (RemoteException e) {
-				Log.e(TAG, e.getLocalizedMessage());
-			}
-		}
+
+	@Override
+	protected boolean isTargetIntent(Context context, Intent intent) {
+		return intent.hasExtra(TelephonyManager.EXTRA_STATE)
+				&& intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+								.equals(TelephonyManager.EXTRA_STATE_RINGING);
+	}
+
+	@Override
+	protected String getType(Context context, Intent intent) {
+		return IN_CALL;
 	}
 }
--- android-prerelise/BlueCharm/src/ru/spbau/bluecharm/SmsNotifier.java
+++ android-prerelise/BlueCharm/src/ru/spbau/bluecharm/SmsNotifier.java
@@ -1,61 +1,28 @@
 package ru.spbau.bluecharm;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.telephony.SmsMessage;
-import android.util.Log;
 
-public class SmsNotifier extends BroadcastReceiver { 
-	public static final String TAG = "SMS_NOTIFIER";
+public class SmsNotifier extends BlueCharmNotifier { 
 	public static final String IN_SMS = "IN_SMS";
-	public static final char DELIMETER = 3;
-	public static final String CHARM = "BLUECHARM";
 	public static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
-	
+
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals(ACTION)) {
-			Log.d(TAG, "Message received");
-			Bundle pudsBundle = intent.getExtras();
-			Object[] pdus = (Object[]) pudsBundle.get("pdus");
-			SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[0]);
-			String body = message.getDisplayMessageBody();
-			String orign = message.getOriginatingAddress();
-			Log.d(TAG, "Message: " + body);
-			Log.d(TAG, "From: " + orign);
-			Intent service = new Intent(context, BlueCharmService.class);
-			IBinder binder = peekService(context, service);
-			if (binder != null) {
-				StringBuilder str = new StringBuilder();
-				str.append(CHARM);
-				str.append(DELIMETER);
-				str.append(IN_SMS);
-				str.append(DELIMETER);
-				str.append(orign);
-				str.append(DELIMETER);
-				str.append(body);
-				sendMessage(str.toString(), binder);
-			} else {
-				Log.d(TAG, "BlueCharmService isn't running");
-			}
-		}
+	protected String buildMessage(Context context, Intent intent) {
+		Bundle pudsBundle = intent.getExtras();
+		Object[] pdus = (Object[]) pudsBundle.get("pdus");
+		SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[0]);
+		return message.getOriginatingAddress() + getDelimiter() + message.getDisplayMessageBody();
 	}
-	
-	private void sendMessage(String sms, IBinder binder) {
-		Messenger messenger = new Messenger(binder);
-		Message msg = Message.obtain(null, BlueCharmService.MSG_NOTIFY_LISTENERS, 0, 0);
-		Bundle bundle = new Bundle();
-		bundle.putString(null, sms);
-		msg.setData(bundle);
-		try {
-			messenger.send(msg);
-		} catch (RemoteException e) {
-			Log.e(TAG, e.getLocalizedMessage());
-		}
+
+	@Override
+	protected String getType(Context context, Intent intent) {
+		return IN_SMS;
+	}
+
+	@Override
+	protected boolean isTargetIntent(Context context, Intent intent) {
+		return intent.getAction().equals(ACTION);
 	}
 }
