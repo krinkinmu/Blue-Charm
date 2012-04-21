cd9efc837cb11858b04e6125056a37b9de61aac2
sergey <sergey@sergey-Aspire-5750G.(none)>
2012-04-21 17:25:52 +0400

    gnome Tray refactoring

--- applets/gnome/tray.py
+++ applets/gnome/tray.py
@@ -12,6 +12,7 @@ class Tray(threading.Thread):
 	__path = "/home/sergey/softpract/Blue-Charm/applets/rc/"
 	__noCallsImageFile = __path + "no_calls.png"
 	__missedCallsImageFile = __path + "missed_call.png"
+	__readImageFile = __path + "checkbox.png"
 	__quitImageFile = __path + "exit.png"
 
 	__tray = gtk.StatusIcon()
@@ -22,8 +23,7 @@ class Tray(threading.Thread):
 		threading.Thread.__init__(self)
 		self.__counter = counter
 
-		self.__tray.connect('button-press-event', self.__tray_icon_callback)
-		self.__dropCounter()
+		self.__tray.connect('button-press-event', self.__openMenu)
 
 		quitImage = gtk.Image()
 		quitImage.set_from_file(self.__quitImageFile)
@@ -35,29 +35,38 @@ class Tray(threading.Thread):
 		quitMenuItem.connect('activate', gtk.main_quit)
 		quitMenuItem.show()
 
+		readImage = gtk.Image()
+		readImage.set_from_file(self.__readImageFile)
+		readImage.show()
+
+		self.__readMenuItem = gtk.ImageMenuItem("Read")
+		self.__readMenuItem.set_image(readImage)
+		self.__readMenuItem.set_always_show_image(True)
+		self.__readMenuItem.connect('activate', self.__readMenuItemCallback)
+		self.__readMenuItem.show()
+
+		self.__menu.append(self.__readMenuItem)
 		self.__menu.append(quitMenuItem)
 
+		self.__dropCounter()
+
+
+	def __readMenuItemCallback(self, event):
+		self.__dropCounter()
 
 	def __dropCounter(self):
 		self.__counter.null()
+		self.__readMenuItem.set_sensitive(False)
 		self.__tray.set_from_file(self.__noCallsImageFile)
 
 
 	def setMessageRecievedImage(self):
+		self.__readMenuItem.set_sensitive(True)
 		self.__tray.set_from_file(self.__missedCallsImageFile)
 
 
-	def __openMenu(self):
+	def __openMenu(self, widget, event):
 		self.__menu.popup(None, None, None, 0, 0)
-
-
-	def __tray_icon_callback(self, widget, event):
-		self.__counter.acquire()
-		if (self.__counter.value() != 0):
-			self.__dropCounter()
-		else:
-			self.__openMenu()
-		self.__counter.release()
 
 
 	def run(self):
