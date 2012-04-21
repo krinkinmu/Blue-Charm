7432fe17d6bdbcf4d6989219d4e6f834c1a894f7
sergey <sergey@sergey-Aspire-5750G.(none)>
2012-04-21 16:56:35 +0400

    gnome Tray refactoring

--- applets/gnome/tray.py
+++ applets/gnome/tray.py
@@ -9,61 +9,56 @@ import counter
 
 class Tray(threading.Thread):
 
+	__path = "/home/sergey/softpract/Blue-Charm/applets/rc/"
+	__noCallsImageFile = __path + "no_calls.png"
+	__missedCallsImageFile = __path + "missed_call.png"
+	__quitImageFile = __path + "exit.png"
+
+	__tray = gtk.StatusIcon()
+	__menu = gtk.Menu()
+
 
 	def __init__(self, counter):
 		threading.Thread.__init__(self)
-		self.counter = counter
+		self.__counter = counter
 
-		path = "/home/sergey/softpract/Blue-Charm/applets/rc/"
-		self.noCallsImageFile = path + "no_calls.png"
-		self.missedCallsImageFile = path + "missed_call.png"
-		quitImageFile = path + "exit.png"
-		
-		self.tray = gtk.StatusIcon()
-		self.tray.connect('button-press-event', self.tray_icon_callback)
-		self.changeImage(self.noCallsImageFile)
-	
-	
-			
+		self.__tray.connect('button-press-event', self.__tray_icon_callback)
+		self.__dropCounter()
+
 		quitImage = gtk.Image()
-		quitImage.set_from_file(quitImageFile)
+		quitImage.set_from_file(self.__quitImageFile)
 		quitImage.show()
 
-		self.menu = gtk.Menu()
-		quit = gtk.ImageMenuItem("exit")
-		quit.set_image(quitImage)
-		quit.set_always_show_image(True)
-		quit.connect('activate', gtk.main_quit)
-		quit.show()
+		quitMenuItem = gtk.ImageMenuItem("exit")
+		quitMenuItem.set_image(quitImage)
+		quitMenuItem.set_always_show_image(True)
+		quitMenuItem.connect('activate', gtk.main_quit)
+		quitMenuItem.show()
 
-		self.menu.append(quit)
-
-
-        
-	def changeImage(self, string):
-		self.tray.set_from_file(string)
+		self.__menu.append(quitMenuItem)
 
 
-	def dropCounter(self):
-		self.counter.null()
-		self.changeImage(self.noCallsImageFile)
+	def __dropCounter(self):
+		self.__counter.null()
+		self.__tray.set_from_file(self.__noCallsImageFile)
+
 
 	def setMessageRecievedImage(self):
-		self.changeImage(self.missedCallsImageFile);
-	
-	def openMenu(self):
-		self.menu.popup(None, None, None, 0, 0)
+		self.__tray.set_from_file(self.__missedCallsImageFile)
 
 
-	def tray_icon_callback(self, widget, event):
-		self.counter.acquire()
-		if (self.counter.value() != 0):
-			self.dropCounter()
+	def __openMenu(self):
+		self.__menu.popup(None, None, None, 0, 0)
+
+
+	def __tray_icon_callback(self, widget, event):
+		self.__counter.acquire()
+		if (self.__counter.value() != 0):
+			self.__dropCounter()
 		else:
-			self.openMenu()
-		self.counter.release()
+			self.__openMenu()
+		self.__counter.release()
 
-	
 
 	def run(self):
 	        gtk.threads_enter()
